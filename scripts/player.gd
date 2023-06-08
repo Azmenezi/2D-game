fa61097bfb278d0f29f3e47113c009b841df2a1b
Azmenezi <azmenezi@gmail.com>
2023-06-08 12:30:57 +0300

    .

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,26 +1,26 @@
 extends CharacterBody2D
 
+@onready var animated_sprite = $AnimatedSprite2D
+@onready var dash = $Dash
+@onready var space_pressed_time = -0.25 
 
-@export var SPEED = 600
+
+@export var normalSpeed = 600
 @export var JUMP_VELOCITY = -700
 @export var gravity = 30
 @export var health = 100
 
+var dashSpeed = normalSpeed * 3
+var dashLength = 0.4
+var dashDirection = 0
 var onGround = 0
-var space_pressed_time: float
-var animated_sprite
-
-var dashing = false
-var dash_duration = 0.2
-var dash_timer = 0.0
-
-func _ready():
-	space_pressed_time = -0.25 
-	animated_sprite = $AnimatedSprite2D
+var speed 
 
 func _physics_process(delta):
 	
 	animationChanger()
+	
+	handleDashing()
 	
 	handleJumping()
 	
@@ -31,25 +31,21 @@ func _physics_process(delta):
 			velocity.y = 1000
 			
 	
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("move_left", "move_right")
-	if direction:
-		velocity.x = direction * SPEED
+	if dash.is_dashing():
+		velocity.x = dashDirection * speed # Use the stored dash direction
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-	if Input.is_action_just_pressed("dash") and !dashing:
-		dashing = true
-		dash_timer = 3.0
-		if direction == -1:
-			velocity.x = -8000
-		elif direction == 1:
-			velocity.x = 8000
-	if dashing:
-		dash_timer -= delta
-		if dash_timer <= 0:
-			dashing = false
-			velocity.x = direction * SPEED
+		if direction:
+			velocity.x = direction * speed  # Update the velocity and dash direction based on input
+			dashDirection = direction
+		else:
+			velocity.x = move_toward(velocity.x, 0, speed)
+			dashDirection = 0  # Reset the dash direction when the player stops moving
+	
+	
 	move_and_slide()
 	
 	
@@ -67,7 +63,7 @@ func animationChanger():
 		print("Crouching to the right")
 		animated_sprite.flip_h = false
 		animated_sprite.play("crouch")
-		SPEED = 300
+		normalSpeed = 300
 	elif  velocity.x < 0 and Input.is_action_pressed("crouch") and velocity.y == 0:
 		print("Crouching to the left")
 		animated_sprite.flip_h = true
@@ -76,35 +72,47 @@ func animationChanger():
 		print("Crouching ")
 		animated_sprite.play("crouch")
 	if Input.is_action_just_released("crouch") or velocity.y != 0:
-		SPEED = 600
+		normalSpeed = 600
+	
 	
 	#Animation checker
-	if velocity.x == 0 and velocity.y == 0 and !Input.is_action_pressed("crouch"):
-		animated_sprite.play("idle")
-	elif  velocity.x == 600 and velocity.y == 0:
+	if   velocity.x == 600 and velocity.y == 0:
 		animated_sprite.flip_h = false
 		animated_sprite.play("running")
 	elif  velocity.x == -600 and velocity.y == 0:
 		animated_sprite.flip_h = true
 		animated_sprite.play("running")
-	elif  velocity.x == 600 and velocity.y < 0:
+	elif dash.is_dashing() and velocity.y == 0 :
+		animated_sprite.play("dashing")
+	elif dash.is_dashing() and velocity.y != 0 :
+		animated_sprite.play("dashing")
+	elif  velocity.x == 600 and velocity.y < 0  and onGround < 2:
 		animated_sprite.flip_h = false
 		animated_sprite.play("jumping up")
-	elif  velocity.x == -600 and velocity.y < 0:
+	elif  velocity.x == -600 and velocity.y < 0 and onGround < 2 :
 		animated_sprite.flip_h = true
 		animated_sprite.play("jumping up")
+	elif  velocity.x == 600 and velocity.y < 0  and onGround == 2:
+		animated_sprite.flip_h = false
+		animated_sprite.play("double jump")
+	elif  velocity.x == -600 and velocity.y < 0 and onGround == 2 :
+		animated_sprite.flip_h = true
+		animated_sprite.play("double jump")
 	elif  velocity.x == 600 and velocity.y > 0:
 		animated_sprite.flip_h = false
 		animated_sprite.play("jumping down")
 	elif  velocity.x == -600 and velocity.y > 0:
 		animated_sprite.flip_h = true
 		animated_sprite.play("jumping down")
-	elif   velocity.y < 0 :
+	elif   velocity.y < 0  and onGround < 2:
 		animated_sprite.play("jumping up")
+	elif  velocity.y < 0 and onGround == 2 :
+		animated_sprite.flip_h = true
+		animated_sprite.play("double jump")
 	elif   velocity.y > 0 :
 		animated_sprite.play("jumping down")
 	elif !Input.is_action_pressed("crouch"):
-		animated_sprite.stop()
+		animated_sprite.play("idle")
 
 func handleJumping():
 	# Handle Jump.
@@ -129,3 +137,8 @@ func handleJumping():
 			space_pressed_time = -0.25
 
 
+func handleDashing():
+	if Input.is_action_just_pressed("dash") and dashDirection != 0:
+		dash.start_dash(dashLength)
+	speed = dashSpeed if dash.is_dashing() else normalSpeed
+	print(dash.is_dashing(),dash.timer)
